0b135d9a8fdd16c178c17494c666df8223979bac
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-17 18:31:34 -0300

    added the create song function in admin controllers

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -1,3 +1,58 @@
-export const createSong = (req, res) => {
-  res.json({ message: "create song" });
+import { Song } from "../models/song.model.js";
+import { Album } from "../models/album.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+// handle file uploads to cloudinary
+const uploadToCloudinary = async (file) => {
+  try {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+      resource_type: "auto",
+    });
+    return result.secure_url;
+  } catch (error) {
+    console.log("Error in uploading files to cloudinary ", error);
+    throw new Error("Error uploading to cloudinary");
+  }
+};
+
+export const createSong = async (req, res) => {
+  try {
+    // make sure all files are provided
+    if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please provide all files." });
+    }
+
+    const { title, artist, duration, albumId } = req.body;
+    const audioFile = req.files.audioFile;
+    const imageFile = req.files.imageFile;
+
+    // upload the files
+    const audioUrl = await uploadToCloudinary(audioFile);
+    const imageUrl = await uploadToCloudinary(imageFile);
+
+    // create the new song
+    const song = new Song({
+      title,
+      artist,
+      imageUrl,
+      audioUrl,
+      duration,
+      albumId: albumId || null,
+    });
+
+    await song.save();
+
+    // add it to its album
+    if (albumId) {
+      await Album.findByIdAndUpdate(albumId, {
+        $push: { songs: song._id },
+      });
+    }
+    res.status(201).json({ success: true, song });
+  } catch (error) {
+    console.log("Error in create song ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -1,12 +1,9 @@
 import express from "express";
-import {
-  protectRoute,
-  requireAdmin,
-} from "../../middlewares/auth.middleware.js";
+import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
 
 import { createSong } from "../controllers/admin.controller.js";
 const router = express.Router();
 
-router.get("/", protectRoute, requireAdmin, createSong);
+router.post("/songs", protectRoute, requireAdmin, createSong);
 
 export default router;
