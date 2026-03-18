026dbde0eb13f7d9f2b6b15273dee7f1841357f4
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-18 16:02:15 -0300

    add the create and delete album controllers

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -84,3 +84,55 @@ export const deleteSong = async (req, res, next) => {
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
+
+export const createAlbum = async (req, res, next) => {
+  try {
+    const { title, artist, releaseYear } = req.body;
+    const { imageFile } = req.files;
+    if (!req.files || !req.files.imageFile)
+      return res.status(400).json({
+        success: false,
+        message: "please provide the image of the album",
+      });
+    const imageUrl = await uploadToCloudinary(imageFile);
+
+    if (!title || !artist || !imageUrl || !releaseYear) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const newAlbum = new Album({
+      title,
+      artist,
+      imageUrl,
+      releaseYear,
+    });
+    await newAlbum.save();
+    res.status(201).json({ success: true, newAlbum });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const deleteAlbum = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const album = await Album.findById(id);
+    if (!album) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Album not found" });
+    }
+    await Song.deleteMany({ albumId: id });
+    await Album.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Album deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
 
-import { createSong, deleteSong } from "../controllers/admin.controller.js";
+import {
+  createSong,
+  deleteSong,
+  createAlbum,
+  deleteAlbum,
+} from "../controllers/admin.controller.js";
 const router = express.Router();
 
 router.post("/songs", protectRoute, requireAdmin, createSong);
 router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+router.post("/albums", protectRoute, requireAdmin, createAlbum);
+router.delete("/albums/:id", protectRoute, requireAdmin, deleteAlbum);
+
 export default router;
