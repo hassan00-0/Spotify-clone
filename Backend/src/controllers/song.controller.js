82de391cc5c5095a311b2d6ee8c114e3b086a195
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-18 21:31:55 -0300

    added the getMadeForYouSongs and getTrendingSongs controllers

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -1,8 +1,18 @@
 import { Song } from "../models/song.model.js";
 
+const songProjection = {
+  $project: {
+    _id: 1,
+    title: 1,
+    artist: 1,
+    imageUrl: 1,
+    audioUrl: 1,
+  },
+};
+
 export const getAllSongs = async (req, res) => {
   try {
-    const songs = await Song.find({}).sort({ createAt: -1 });
+    const songs = await Song.find({}).sort({ createdAt: -1 });
     if (!songs) {
       return res
         .status(404)
@@ -11,32 +21,53 @@ export const getAllSongs = async (req, res) => {
     res.status(200).json({ success: true, songs });
   } catch (error) {
     console.log("error in getAllSongs ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
 export const getFeaturedSongs = async (req, res) => {
   try {
+    // fetch 6 random songs
     const songs = await Song.aggregate([
       {
         $sample: { size: 6 },
       },
-      {
-        $project: {
-          _id: 1,
-          title: 1,
-          artist: 1,
-          imageUrl: 1,
-          audioUrl: 1,
-        },
-      },
+      songProjection,
     ]);
     res.json({ success: true, songs });
   } catch (error) {
     console.log("error in getting featured songs ", error);
-    res.status(500).json({ success: true, message: "Internal Server Error" });
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
-export const getMadeForYouSongs = async (req, res) => {};
+export const getMadeForYouSongs = async (req, res) => {
+  try {
+    // fetch 4 random songs
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      songProjection,
+    ]);
+    res.json({ success: true, songs });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
-export const getTrendingSongs = async (req, res) => {};
+export const getTrendingSongs = async (req, res) => {
+  try {
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      songProjection,
+    ]);
+    res.json({ success: true, songs });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
