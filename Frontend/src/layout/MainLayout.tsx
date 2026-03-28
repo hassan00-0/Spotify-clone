a1c82e780358ee45ee8a8cf14e1ec522e6bac4a8
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-28 17:48:07 -0300

    fix some spacing issues in the sidebars

--- Frontend/src/layout/MainLayout.tsx
+++ Frontend/src/layout/MainLayout.tsx
@@ -1,27 +1,35 @@
-import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   const isMobile = false;
   return (
-    <div className="h-screen bg-black text-white flex flex-col ">
+    <div className="h-screen bg-black text-white flex flex-col">
       <ResizablePanelGroup
         orientation="horizontal"
-        className="flex-1 flex h-full overflow-hidden p-2"
+        className="flex-1 flex h-full overflow-hidden p-2 w-full min-w-0"
       >
         {/* left side bar */}
         <ResizablePanel
           defaultSize={20}
           minSize={isMobile ? 0 : 10}
           maxSize={350}
+          className="overflow-hidden min-w-0"
         >
-          left sidebar
+          <div className="truncate">left sidebar</div>
         </ResizablePanel>
 
+        <ResizableHandle className="w-2 bg-black rounded-lg" />
         {/* the main content */}
         <ResizablePanel defaultSize={isMobile ? 80 : 60}>
           <Outlet />
         </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg" />
 
         {/* right side bar */}
         <ResizablePanel
@@ -29,8 +37,9 @@ const MainLayout = () => {
           minSize={0}
           maxSize={310}
           collapsedSize={0}
+          className="overflow-hidden min-w-0"
         >
-          friends activity
+          <div className="truncate">friends activity</div>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
