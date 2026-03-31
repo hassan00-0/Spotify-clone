983eb35b0df2358c134c019f2833ef8fc5586e63
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-31 20:23:40 -0300

    make the lower table of songs part of the album page

--- Frontend/src/pages/album/AlbumPage.tsx
+++ Frontend/src/pages/album/AlbumPage.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useMusicStore } from "@/stores/useMusicStore.ts";
+import { Clock, Play } from "lucide-react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+const formatDuration = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+};
 
 const AlbumPage = () => {
   const { albumId } = useParams();
@@ -45,10 +53,64 @@ const AlbumPage = () => {
                 </div>
               </div>
             </div>
+
+            {/* playbutton */}
+            <div className="flex items-center">
+              <Button className="size-14 rounded-full bg-green-500 hover:bg-green-400 hover:scale-105 transition-all">
+                <Play className="size-7 text-black" />
+              </Button>
+            </div>
           </div>
         </div>
         {/* lower half */}
-        <div className=""></div>
+        <div className="bg-black/20 backdrop-blur-sm">
+          {/* table header */}
+          <div
+            className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2
+          text-sm text-zinc-400 border-b border-white/5"
+          >
+            <div>#</div>
+            <div>Title</div>
+            <div>Released Date</div>
+            <div>
+              <Clock className="size-4" />
+            </div>
+          </div>
+
+          {/* songs list */}
+          <div className="space-y-2">
+            {currentAlbum?.songs.map((song, index) => (
+              <div
+                key={song._id}
+                className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 group cursor-pointer"
+              >
+                <div className="flex items-center justify-center">
+                  <div className="group-hover:hidden">{index + 1}</div>
+                  <Play className="hidden group-hover:block size-4" />
+                </div>
+
+                <div className="flex items-center gap-3">
+                  <img
+                    src={song.imageUrl}
+                    alt={song.title}
+                    className="size-10"
+                  />
+
+                  <div>
+                    <div className="font-medium ">{song.title}</div>
+                    <div>{song.artist}</div>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  {song.createdAt.split("T")[0]}
+                </div>
+                <div className="flex items-center">
+                  {formatDuration(song.duration)}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </ScrollArea>
   );
